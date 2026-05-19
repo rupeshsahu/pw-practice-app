12d43b1cb98e9e8e0f7e5e448434df8ee28496c5
Rupesh Sahu <rsahu2@opentext.com>
2026-05-19 14:19:26 +0530

    done till extracting values

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test'
+import {expect, test} from '@playwright/test'
 test.beforeEach(async ({page}) => {
   await page.goto("http://localhost:4200/")
   await page.getByText('Forms').click()
@@ -54,6 +54,42 @@ test('parent locators',async ({page})=>{
   await page.locator('nb-card',{hasText:"Form without labels"}).getByRole('textbox',{name:"Subject"}).fill("rupesh")
   await page.locator('nb-card',{hasText:"Form without labels"}).getByRole('textbox',{name:"Message"}).fill("rupesh")
   await page.locator('nb-card',{hasText:"Form without labels"}).getByRole('button',{name:"SEND"}).click()
+})
+
+test('Reusing locators',async({page})=>{
+  const BlockForm=page.locator('nb-card').filter({hasText:"Block form"})
+  const FirstNameTextBox=BlockForm.getByRole('textbox',{name:"First Name"})
+  const Email=BlockForm.getByRole('textbox',{name:"Email"})
+  const website=BlockForm.getByRole('textbox',{name:"Website"})
+  const LastNameTextBox=BlockForm.getByRole('textbox',{name:"Last Name"})
+  const submitButton=BlockForm.getByRole('button',{name:"SUBMIT"})
+   await BlockForm.click()
+  await FirstNameTextBox.fill("Rupesh")
+  await LastNameTextBox.fill("sahu")
+  await Email.fill("rupesh@gmail.com")
+  await website.first().fill("www.rupeshsahu.com")
+  await submitButton.click()
+
+  await expect(await FirstNameTextBox.inputValue()).toEqual("Rupesh")
+  const allTextBoxes=await BlockForm.getByRole('textbox').all()
+  console.log(allTextBoxes.length)
 
 
 })
+
+test('Extracting values',async({page})=> {
+  const radioButtonsOfForm=page.locator('nb-radio').allTextContents()
+  await expect(await radioButtonsOfForm).toContain("Option 1")
+  const usingTheGridForm=page.locator('nb-card').filter({hasText:"Using the Grid"})
+  const emailPlaceholder=await usingTheGridForm.getByRole('textbox',{name:"Email"}).getAttribute("placeholder")
+  await expect(emailPlaceholder).toEqual("Email")
+  const buttonText=await usingTheGridForm.getByRole('button').textContent()
+  await expect(buttonText).toEqual("Sign in")
+
+  await usingTheGridForm.getByRole('textbox',{name:"Email"}).fill("rupesh@opentext.com")
+  const textboxVaule=await usingTheGridForm.getByRole('textbox',{name:"Email"}).inputValue()
+  await expect(textboxVaule).toEqual("rupesh@opentext.com")
+
+
+})
+
